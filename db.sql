defb9e623fab80bcae2e38331d1442a13591ab53
Justin C. Klein Keane <klein_keanej@mlhs.org>
2017-01-18 10:01:21 -0500

    Modified permissions for admin and mailer on the token table.

--- db.sql
+++ db.sql
@@ -246,13 +246,15 @@ GRANT SELECT ON stumblephish.group TO 'sp_mailer'@'localhost';
 GRANT SELECT ON stumblephish.target_x_group TO 'sp_mailer'@'localhost';
 GRANT SELECT ON stumblephish.target TO 'sp_mailer'@'localhost';
 GRANT SELECT ON stumblephish.email_template TO 'sp_mailer'@'localhost';
-GRANT SELECT,INSERT ON stumblephish.target TO 'sp_mailer'@'localhost';
+GRANT SELECT ON stumblephish.target TO 'sp_mailer'@'localhost';
+GRANT SELECT,INSERT ON stumblephish.token TO 'sp_mailer'@'localhost';
 GRANT INSERT ON stumblephish.logs TO 'sp_mailer'@'localhost';
 GRANT INSERT ON stumblephish.mailer TO 'sp_mailer'@'localhost';
 -- Website front end account
 GRANT SELECT ON stumblephish.web_template TO 'sp_website'@'localhost';
 GRANT INSERT ON stumblephish.logs TO 'sp_website'@'localhost';
 GRANT INSERT ON stumblephish.bite TO 'sp_website'@'localhost';
+GRANT SELECT ON stumblephish.token TO 'sp_website'@'localhost';
 -- Administrative interface account
 GRANT SELECT,INSERT,UPDATE,DELETE ON stumblephish.campaign TO 'sp_reportadmin'@'localhost';
 GRANT SELECT,INSERT,UPDATE,DELETE ON stumblephish.campaign_x_group TO 'sp_reportadmin'@'localhost';
@@ -262,7 +264,7 @@ GRANT SELECT,INSERT,UPDATE,DELETE ON stumblephish.target TO 'sp_reportadmin'@'lo
 GRANT SELECT,INSERT,UPDATE,DELETE ON stumblephish.email_template TO 'sp_reportadmin'@'localhost';
 GRANT SELECT,INSERT,UPDATE,DELETE ON stumblephish.web_template TO 'sp_reportadmin'@'localhost';
 GRANT SELECT,INSERT,UPDATE,DELETE ON stumblephish.target TO 'sp_reportadmin'@'localhost';
-GRANT SELECT ON stumblephish.token TO 'sp_reportadmin'@'localhost';
+GRANT SELECT stumblephish.token TO 'sp_reportadmin'@'localhost';
 GRANT SELECT ON stumblephish.bite TO 'sp_reportadmin'@'localhost';
 GRANT SELECT,INSERT ON stumblephish.logs TO 'sp_reportadmin'@'localhost';
 GRANT SELECT ON stumblephish.mailer TO 'sp_reportadmin'@'localhost';
