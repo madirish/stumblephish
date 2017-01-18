bc446e56425a68a1bef92dd519a8072ebae3d5f9
Justin C. Klein Keane <klein_keanej@mlhs.org>
2017-01-18 13:10:35 -0500

    Moving user permissions to new file to distinguish operations

--- db.sql
+++ db.sql
@@ -15,8 +15,6 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 CREATE SCHEMA IF NOT EXISTS `stumblephish` DEFAULT CHARACTER SET utf8 ;
 USE `stumblephish` ;
 
-
-
 -- -----------------------------------------------------
 -- Table `stumblephish`.`target`
 -- -----------------------------------------------------
@@ -227,47 +225,6 @@ CREATE TABLE IF NOT EXISTS `stumblephish`.`mailer` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 COMMENT = 'Logs from the mailer application and any SMTP feedback.';
-
--- -----------------------------------------------------
--- Create user accounts
--- -----------------------------------------------------
-
-CREATE USER 'sp_admin'@'localhost' IDENTIFIED BY 'change_stumblephish_password';
-CREATE USER 'sp_mailer'@'localhost' IDENTIFIED BY 'change_stumblephish_password';
-CREATE USER 'sp_website'@'localhost' IDENTIFIED BY 'change_stumblephish_password';
-CREATE USER 'sp_reportadmin'@'localhost' IDENTIFIED BY 'change_stumblephish_password';
-
--- Superadmin/developer account
-GRANT ALL ON stumblephish.* TO 'sp_admin'@'localhost';
--- Mailer account
-GRANT SELECT ON stumblephish.campaign TO 'sp_mailer'@'localhost';
-GRANT SELECT ON stumblephish.campaign_x_group TO 'sp_mailer'@'localhost';
-GRANT SELECT ON stumblephish.group TO 'sp_mailer'@'localhost';
-GRANT SELECT ON stumblephish.target_x_group TO 'sp_mailer'@'localhost';
-GRANT SELECT ON stumblephish.target TO 'sp_mailer'@'localhost';
-GRANT SELECT ON stumblephish.email_template TO 'sp_mailer'@'localhost';
-GRANT SELECT ON stumblephish.target TO 'sp_mailer'@'localhost';
-GRANT SELECT,INSERT ON stumblephish.token TO 'sp_mailer'@'localhost';
-GRANT INSERT ON stumblephish.logs TO 'sp_mailer'@'localhost';
-GRANT INSERT ON stumblephish.mailer TO 'sp_mailer'@'localhost';
--- Website front end account
-GRANT SELECT ON stumblephish.web_template TO 'sp_website'@'localhost';
-GRANT INSERT ON stumblephish.logs TO 'sp_website'@'localhost';
-GRANT INSERT ON stumblephish.bite TO 'sp_website'@'localhost';
-GRANT SELECT ON stumblephish.token TO 'sp_website'@'localhost';
--- Administrative interface account
-GRANT SELECT,INSERT,UPDATE,DELETE ON stumblephish.campaign TO 'sp_reportadmin'@'localhost';
-GRANT SELECT,INSERT,UPDATE,DELETE ON stumblephish.campaign_x_group TO 'sp_reportadmin'@'localhost';
-GRANT SELECT,INSERT,UPDATE,DELETE ON stumblephish.group TO 'sp_reportadmin'@'localhost';
-GRANT SELECT,INSERT,UPDATE,DELETE ON stumblephish.target_x_group TO 'sp_reportadmin'@'localhost';
-GRANT SELECT,INSERT,UPDATE,DELETE ON stumblephish.target TO 'sp_reportadmin'@'localhost';
-GRANT SELECT,INSERT,UPDATE,DELETE ON stumblephish.email_template TO 'sp_reportadmin'@'localhost';
-GRANT SELECT,INSERT,UPDATE,DELETE ON stumblephish.web_template TO 'sp_reportadmin'@'localhost';
-GRANT SELECT,INSERT,UPDATE,DELETE ON stumblephish.target TO 'sp_reportadmin'@'localhost';
-GRANT SELECT stumblephish.token TO 'sp_reportadmin'@'localhost';
-GRANT SELECT ON stumblephish.bite TO 'sp_reportadmin'@'localhost';
-GRANT SELECT,INSERT ON stumblephish.logs TO 'sp_reportadmin'@'localhost';
-GRANT SELECT ON stumblephish.mailer TO 'sp_reportadmin'@'localhost';
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
---
+++ user-perms.sql
@@ -0,0 +1,40 @@
+-- -----------------------------------------------------
+-- Create user accounts
+-- -----------------------------------------------------
+
+CREATE USER 'sp_admin'@'localhost' IDENTIFIED BY 'change_stumblephish_password';
+CREATE USER 'sp_mailer'@'localhost' IDENTIFIED BY 'change_stumblephish_password';
+CREATE USER 'sp_website'@'localhost' IDENTIFIED BY 'change_stumblephish_password';
+CREATE USER 'sp_reportadmin'@'localhost' IDENTIFIED BY 'change_stumblephish_password';
+
+-- Superadmin/developer account
+GRANT ALL ON stumblephish.* TO 'sp_admin'@'localhost';
+-- Mailer account
+GRANT SELECT ON stumblephish.campaign TO 'sp_mailer'@'localhost';
+GRANT SELECT ON stumblephish.campaign_x_group TO 'sp_mailer'@'localhost';
+GRANT SELECT ON stumblephish.group TO 'sp_mailer'@'localhost';
+GRANT SELECT ON stumblephish.target_x_group TO 'sp_mailer'@'localhost';
+GRANT SELECT ON stumblephish.target TO 'sp_mailer'@'localhost';
+GRANT SELECT ON stumblephish.email_template TO 'sp_mailer'@'localhost';
+GRANT SELECT ON stumblephish.target TO 'sp_mailer'@'localhost';
+GRANT SELECT,INSERT ON stumblephish.token TO 'sp_mailer'@'localhost';
+GRANT INSERT ON stumblephish.logs TO 'sp_mailer'@'localhost';
+GRANT INSERT ON stumblephish.mailer TO 'sp_mailer'@'localhost';
+-- Website front end account
+GRANT SELECT ON stumblephish.web_template TO 'sp_website'@'localhost';
+GRANT INSERT ON stumblephish.logs TO 'sp_website'@'localhost';
+GRANT INSERT ON stumblephish.bite TO 'sp_website'@'localhost';
+GRANT SELECT ON stumblephish.token TO 'sp_website'@'localhost';
+-- Administrative interface account
+GRANT SELECT,INSERT,UPDATE,DELETE ON stumblephish.campaign TO 'sp_reportadmin'@'localhost';
+GRANT SELECT,INSERT,UPDATE,DELETE ON stumblephish.campaign_x_group TO 'sp_reportadmin'@'localhost';
+GRANT SELECT,INSERT,UPDATE,DELETE ON stumblephish.group TO 'sp_reportadmin'@'localhost';
+GRANT SELECT,INSERT,UPDATE,DELETE ON stumblephish.target_x_group TO 'sp_reportadmin'@'localhost';
+GRANT SELECT,INSERT,UPDATE,DELETE ON stumblephish.target TO 'sp_reportadmin'@'localhost';
+GRANT SELECT,INSERT,UPDATE,DELETE ON stumblephish.email_template TO 'sp_reportadmin'@'localhost';
+GRANT SELECT,INSERT,UPDATE,DELETE ON stumblephish.web_template TO 'sp_reportadmin'@'localhost';
+GRANT SELECT,INSERT,UPDATE,DELETE ON stumblephish.target TO 'sp_reportadmin'@'localhost';
+GRANT SELECT stumblephish.token TO 'sp_reportadmin'@'localhost';
+GRANT SELECT ON stumblephish.bite TO 'sp_reportadmin'@'localhost';
+GRANT SELECT,INSERT ON stumblephish.logs TO 'sp_reportadmin'@'localhost';
+GRANT SELECT ON stumblephish.mailer TO 'sp_reportadmin'@'localhost';
